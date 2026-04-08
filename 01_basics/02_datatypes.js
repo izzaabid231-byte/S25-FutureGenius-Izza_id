555b8f655870980c26451441fc1c1e568bda0ace
izzaabid231-byte <izzaabid231@gmail.com>
2026-04-08 18:28:53 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+3)  //we are using nodejs,not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+
+console.log("Paroooo")
+
+//some datatypes
+let name = "Naboo"
+let age = 46
+let phonenumber = true
+
+
+// number => (range) 2 to power 52
+// bigint for  large number for example in trading or some big website like amazon
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value can't assign 
+// symbol =>  use for uniqueness
+// object 
+
+
+console.log(typeof null) // output=> object ( because it's an object not data type)
+console.log(typeof undefined) // output=> udefined (because it'sa data type not object )
