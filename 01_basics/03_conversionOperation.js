dee8c828922633499e71807df664d4cab72ff8a6
izzaabid231-byte <izzaabid231@gmail.com>
2026-04-09 13:25:46 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,6 @@
 let score = "Izza"
-
 // console.log(typeof score)
 // console.log(typeof (score))
-
 
 
 let valueInNumber = Number(score)
@@ -24,5 +22,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 44
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************  Operations  ************************
+
+let value = 4
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2) => remainder
+
+let str1 = "operations"
+let str2 = "Izza"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)  //if strings comes in start remaining numbers also treated as a string
+// console.log(1 + 2 + "2")  //if strind comes at end remaining numbers cannot treated as a string 
+
+// console.log(3 + 4 * 5 % 3); //this is wrong way to write a program 
+// console.log(((3 + 4) * 5) % 3); //use brackets
+
+
+// console.log(+true);  //wrong way
+// console.log(+"");  //wrong way
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameConter = 100
+++gameConter;
+console.log(gameConter);
