82c53f37725671bd99f40f5782dcdb0aa5e4f3c2
izzaabid231-byte <izzaabid231@gmail.com>
2026-05-07 20:33:53 +0000

    while/do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+//while loop
+
+// let index = 1
+// while (index <= 10 ) {
+//     //console.log(`Value od index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = ["batman", "supernam", "thor", "hulk", "spiderman"]
+// let arry = 0
+// while (arry < myArray.lenght) {
+//     //console.log(`Value is ${myArray[arry]}`);
+//     arry = array + 1
+    
+// }
+
+//do while
+
+let score = 31
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 25);
